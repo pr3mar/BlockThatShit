22bf202abea7e539aec3f2c115da143251ac1421
Alex Stefanovski <aleksandar.stefanovski.2013@students.finki.ukim.mk>
2015-01-15 18:12:30 +0100

    added ability to minimize to tray

--- BlockerApp/MainForm.cpp
+++ BlockerApp/MainForm.cpp
@@ -4,7 +4,8 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
-void main(array<String^>^ args) {
+void main(array<String^>^ args) 
+{
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
--- BlockerApp/MainForm.h
+++ BlockerApp/MainForm.h
@@ -34,12 +34,26 @@ namespace BlockerApp {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  blockToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  aboutToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  exitToolStripMenuItem1;
+	private: System::Windows::Forms::ToolStripMenuItem^  aboutToolStripMenuItem1;
+	private: System::Windows::Forms::ToolStripMenuItem^  exitToolStripMenuItem;
+	private: System::Windows::Forms::TabControl^  tabControl1;
+	private: System::Windows::Forms::TabPage^  tabPage1;
+	private: System::Windows::Forms::TabPage^  tabPage2;
+	private: System::Windows::Forms::NotifyIcon^  notifyIcon1;
+	private: System::ComponentModel::IContainer^  components;
+
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,18 +62,162 @@ namespace BlockerApp {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MainForm::typeid));
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->fileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->blockToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->aboutToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->exitToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->aboutToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->exitToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->notifyIcon1 = (gcnew System::Windows::Forms::NotifyIcon(this->components));
+			this->menuStrip1->SuspendLayout();
+			this->tabControl1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->fileToolStripMenuItem,
+					this->aboutToolStripMenuItem1, this->exitToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(784, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this->fileToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->blockToolStripMenuItem,
+					this->aboutToolStripMenuItem, this->exitToolStripMenuItem1
+			});
+			this->fileToolStripMenuItem->Name = L"fileToolStripMenuItem";
+			this->fileToolStripMenuItem->Size = System::Drawing::Size(37, 20);
+			this->fileToolStripMenuItem->Text = L"File";
+			// 
+			// blockToolStripMenuItem
+			// 
+			this->blockToolStripMenuItem->Name = L"blockToolStripMenuItem";
+			this->blockToolStripMenuItem->Size = System::Drawing::Size(106, 22);
+			this->blockToolStripMenuItem->Text = L"Open";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this->aboutToolStripMenuItem->Name = L"aboutToolStripMenuItem";
+			this->aboutToolStripMenuItem->Size = System::Drawing::Size(106, 22);
+			this->aboutToolStripMenuItem->Text = L"Block!";
+			// 
+			// exitToolStripMenuItem1
+			// 
+			this->exitToolStripMenuItem1->Name = L"exitToolStripMenuItem1";
+			this->exitToolStripMenuItem1->Size = System::Drawing::Size(106, 22);
+			this->exitToolStripMenuItem1->Text = L"Exit";
+			// 
+			// aboutToolStripMenuItem1
+			// 
+			this->aboutToolStripMenuItem1->Name = L"aboutToolStripMenuItem1";
+			this->aboutToolStripMenuItem1->Size = System::Drawing::Size(52, 20);
+			this->aboutToolStripMenuItem1->Text = L"About";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this->exitToolStripMenuItem->Name = L"exitToolStripMenuItem";
+			this->exitToolStripMenuItem->Size = System::Drawing::Size(37, 20);
+			this->exitToolStripMenuItem->Text = L"Exit";
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Location = System::Drawing::Point(0, 27);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(791, 536);
+			this->tabControl1->TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->Location = System::Drawing::Point(4, 22);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(783, 510);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"tabPage1";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Location = System::Drawing::Point(4, 22);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(783, 510);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"tabPage2";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// notifyIcon1
+			// 
+			this->notifyIcon1->BalloonTipText = L"Maximize Blocker";
+			this->notifyIcon1->BalloonTipTitle = L"Blocker";
+			this->notifyIcon1->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"notifyIcon1.Icon")));
+			this->notifyIcon1->Text = L"Blocker";
+			this->notifyIcon1->MouseDoubleClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MainForm::notifyIcon1_MouseDoubleClick);
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(620, 429);
+			this->ClientSize = System::Drawing::Size(784, 562);
+			this->Controls->Add(this->tabControl1);
+			this->Controls->Add(this->menuStrip1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MainMenuStrip = this->menuStrip1;
+			this->MaximizeBox = false;
 			this->Name = L"MainForm";
-			this->Text = L"MainForm";
+			this->Text = L"Blocker";
+			this->Resize += gcnew System::EventHandler(this, &MainForm::MainForm_Resize);
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->tabControl1->ResumeLayout(false);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+
+
+		//check if window is open - if yes, when minimized the tray icon is visible
+		//and the form is hidden
+
+	private: System::Void MainForm_Resize(System::Object^  sender, System::EventArgs^  e)
+	{
+		notifyIcon1->BalloonTipTitle = "Minimized to tray!";
+		notifyIcon1->BalloonTipText = "Blocker has been minimized to the system tray. It will continue to run in the background!";
+	
+		if (FormWindowState::Minimized == this->WindowState)
+		{
+			notifyIcon1->Visible = true;
+			notifyIcon1->ShowBalloonTip(500);
+			this->Hide();
+		}
+
+		else if (FormWindowState::Normal == this->WindowState)
+		{
+			notifyIcon1->Visible = false;
+		}
+	}
+
+	private: System::Void notifyIcon1_MouseDoubleClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) 
+	{
+		this->Show();
+		this->WindowState = FormWindowState::Normal;
+	}
+};
+
 }
